cb1f3cfa678d07c8710284aeab269d87cbe17c58
Isaac <isaacryan2703@gmail.com>
2025-01-21 13:30:27 +0800

    Update shoppay.sql

--- shoppay.sql
+++ shoppay.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 20, 2025 at 02:55 PM
+-- Generation Time: Jan 21, 2025 at 06:29 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,19 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `shoppay`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `audit_log`
---
-
-CREATE TABLE `audit_log` (
-  `LogID` int(11) NOT NULL,
-  `UserID` int(11) NOT NULL,
-  `Action` varchar(255) NOT NULL,
-  `ActionDate` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -72,18 +59,11 @@ CREATE TABLE `coupon` (
 
 CREATE TABLE `messages` (
   `MessageID` int(11) NOT NULL,
-  `SenderID` int(11) NOT NULL,
-  `ReceiverID` int(11) NOT NULL,
+  `SenderID` int(11) DEFAULT NULL,
+  `ReceiverID` int(11) DEFAULT NULL,
   `MessageText` text NOT NULL,
   `Timestamp` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `messages`
---
-
-INSERT INTO `messages` (`MessageID`, `SenderID`, `ReceiverID`, `MessageText`, `Timestamp`) VALUES
-(64, 6, 3, 'Hi sir, may i know when my item will be accept ?', '2025-01-20 12:50:04');
 
 -- --------------------------------------------------------
 
@@ -95,9 +75,8 @@ CREATE TABLE `orders` (
   `OrderID` int(11) NOT NULL,
   `UserID` int(11) DEFAULT NULL,
   `TotalAmount` double NOT NULL,
-  `OrderStatus` varchar(50) NOT NULL,
-  `OrderDate` timestamp NOT NULL DEFAULT current_timestamp(),
-  `StatusID` int(11) NOT NULL
+  `StatusID` int(11) DEFAULT NULL,
+  `OrderDate` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -108,12 +87,12 @@ CREATE TABLE `orders` (
 
 CREATE TABLE `order_items` (
   `OrderItemID` int(11) NOT NULL,
-  `OrderID` int(11) NOT NULL,
-  `ProductID` int(11) NOT NULL,
+  `OrderID` int(11) DEFAULT NULL,
+  `ProductID` int(11) DEFAULT NULL,
   `Quantity` int(11) NOT NULL,
   `UnitPrice` double NOT NULL,
-  `ProductName` varchar(100) NOT NULL,
-  `StoreName` varchar(100) NOT NULL
+  `ProductName` varchar(100) DEFAULT NULL,
+  `StoreName` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -127,6 +106,16 @@ CREATE TABLE `order_status` (
   `StatusName` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `order_status`
+--
+
+INSERT INTO `order_status` (`StatusID`, `StatusName`) VALUES
+(1, 'Pending Seller Approval'),
+(2, 'Completed'),
+(3, 'Pending Payment'),
+(4, 'Cancelled');
+
 -- --------------------------------------------------------
 
 --
@@ -135,19 +124,28 @@ CREATE TABLE `order_status` (
 
 CREATE TABLE `product` (
   `ProductID` int(11) NOT NULL,
+  `SellerID` int(11) DEFAULT NULL,
   `ProductName` varchar(100) NOT NULL,
   `Description` text DEFAULT NULL,
   `Price` double NOT NULL,
   `StockQuantity` int(11) NOT NULL,
-  `SellerID` int(11) DEFAULT NULL,
+  `StoreName` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`ProductID`, `ProductName`, `Description`, `Price`, `StockQuantity`, `SellerID`) VALUES
-(1, 'EARBUDS', 'MODEL TYS 110', 25, 47, 3);
+INSERT INTO `product` (`ProductID`, `SellerID`, `ProductName`, `Description`, `Price`, `StockQuantity`, `StoreName`) VALUES
+(1, 3, 'EARBUDS', 'MODEL TYS 110', 25, 32, NULL),
+(3, 3, 'KEYBOARD', 'GREEN SWITCH', 50, 65, NULL),
+(5, 3, 'STYLUS PEN', 'COMPATIBLE WITH ALL DEVICE. BLUETOOTH 5.0v.', 13, 84, NULL),
+(6, 4, 'GRILL X42', 'COMPLETE SET WITH ADDITIONAL TOOLS', 70, 45, NULL),
+(7, 4, 'CLAY POT', 'BRING NATURE TO YOUR HOUSE', 3, 209, NULL),
+(8, 4, 'LADDER 7FT', 'NEW DESIGN WITH GREATER STRENGTH', 24, 85, NULL),
+(9, 7, 'PORTABLE HANDHELD', 'TURBO FAN 100 WIND', 34, 130, NULL),
+(10, 7, 'MINI FAN', '3000mAH USB CLIP FAN ROTATING', 12, 53, NULL),
+(11, 7, 'MINI POWERBANK', '10000mAH HOT PORTABLE WIRESLESS', 89, 5, NULL);
 
 -- --------------------------------------------------------
 
@@ -156,6 +154,7 @@ INSERT INTO `product` (`ProductID`, `ProductName`, `Description`, `Price`, `Stoc
 --
 
 CREATE TABLE `system_settings` (
+  `SettingID` int(11) NOT NULL,
   `SettingName` varchar(50) NOT NULL,
   `Value` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -164,8 +163,8 @@ CREATE TABLE `system_settings` (
 -- Dumping data for table `system_settings`
 --
 
-INSERT INTO `system_settings` (`SettingName`, `Value`) VALUES
-('CommissionRate', 0.02);
+INSERT INTO `system_settings` (`SettingID`, `SettingName`, `Value`) VALUES
+(1, 'CommissionRate', 0.05);
 
 -- --------------------------------------------------------
 
@@ -191,28 +190,28 @@ CREATE TABLE `user` (
   `UserID` int(11) NOT NULL,
   `Username` varchar(50) NOT NULL,
   `Email` varchar(100) NOT NULL,
-  `Password` varchar(50) NOT NULL,
-  `FullName` varchar(100) NOT NULL,
-  `Address` varchar(255) NOT NULL,
-  `PhoneNumber` varchar(15) NOT NULL,
+  `Password` varchar(100) NOT NULL,
+  `FullName` varchar(100) DEFAULT NULL,
+  `Address` varchar(255) DEFAULT NULL,
+  `PhoneNumber` varchar(20) DEFAULT NULL,
   `UserRole` int(11) NOT NULL,
-  `isApproved` tinyint(1) NOT NULL DEFAULT 0,
-  `RegistrationDate` timestamp NOT NULL DEFAULT current_timestamp(),
-  `StoreName` varchar(100) DEFAULT NULL
+  `isApproved` tinyint(1) DEFAULT 0,
+  `StoreName` varchar(100) DEFAULT NULL,
+  `RegistrationDate` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`UserID`, `Username`, `Email`, `Password`, `FullName`, `Address`, `PhoneNumber`, `UserRole`, `isApproved`, `RegistrationDate`, `StoreName`) VALUES
-(1, 'Admin', 'Admin1@gmail.com', 'hashed_123', 'Isaac [Admin]', '', '014 690 5325', 2, 0, '2025-01-19 11:15:17', ''),
-(2, 'Admin2', 'Admin2@gmail.com', 'hashed_123', 'Mirul [Admin]', '', '013 233 4455', 2, 0, '2025-01-19 11:17:42', ''),
-(3, 'Seller', 'Seller1@gmail.com', 'hashed_abc123', 'Abdul Kazim', '', '013 232 3344', 1, 1, '2025-01-19 11:18:28', 'UGREEN'),
-(4, 'Seller2', 'seller2', 'hashed_123', 'Tan Cheng', 'UTeM', '013 445 3232', 1, 1, '2025-01-19 11:19:42', 'DIY'),
-(5, 'Veron', 'veron@gmail.com', 'hashed_123', 'Veron Jr', '', '012 332 2345', 0, 0, '2025-01-19 12:31:41', ''),
-(6, 'Isaac', 'isaac', 'hashed_123', 'Isaac Ryan', 'Taman Tambun, Melaka', '014 225 3485', 0, 0, '2025-01-19 12:32:24', ''),
-(7, 'Seller3', 'seller3@gmail.com', 'hashed_123', 'Anis Hazuki', '', '012 323 4343', 1, 1, '2025-01-19 15:23:27', 'GOJODOQ');
+INSERT INTO `user` (`UserID`, `Username`, `Email`, `Password`, `FullName`, `Address`, `PhoneNumber`, `UserRole`, `isApproved`, `StoreName`, `RegistrationDate`) VALUES
+(1, 'Admin', 'Admin1@gmail.com', 'hashed_123', 'Isaac [Admin]', '', '014 690 5325', 2, 0, '', '2025-01-19 03:15:17'),
+(2, 'Admin2', 'Admin2@gmail.com', 'hashed_123', 'Mirul [Admin]', '', '013 233 4455', 2, 0, '', '2025-01-19 03:17:42'),
+(3, 'Seller', 'Seller1@gmail.com', 'hashed_123', 'Abdul Kazim', '', '013 232 3344', 1, 1, 'UGREEN', '2025-01-19 03:18:28'),
+(4, 'Seller2', 'seller2', 'hashed_123', 'Tan Cheng', 'UTeM', '013 445 3232', 1, 1, 'DIY', '2025-01-19 03:19:42'),
+(5, 'Veron', 'veron@gmail.com', 'hashed_123', 'Veron Jr', '', '012 332 2345', 0, 0, '', '2025-01-19 04:31:41'),
+(6, 'Isaac', 'isaac', 'hashed_123', 'Isaac Ryan', 'Taman Tambun, Melaka', '014 225 3485', 0, 0, '', '2025-01-19 04:32:24'),
+(7, 'Seller3', 'seller3@gmail.com', 'hashed_123', 'Anis Hazuki', '', '012 323 4343', 1, 1, 'GOJODOQ', '2025-01-19 07:23:27');
 
 -- --------------------------------------------------------
 
@@ -231,32 +230,14 @@ CREATE TABLE `wallet` (
 --
 
 INSERT INTO `wallet` (`WalletID`, `UserID`, `Balance`) VALUES
-(1, 6, 140);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `wallet_transaction`
---
-
-CREATE TABLE `wallet_transaction` (
-  `TransactionID` int(11) NOT NULL,
-  `WalletID` int(11) NOT NULL,
-  `Amount` double NOT NULL,
-  `TransactionType` varchar(50) NOT NULL,
-  `TransactionDate` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+(1, 6, 36),
+(2, 3, 100),
+(3, 4, 120),
+(4, 7, 40);
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `audit_log`
---
-ALTER TABLE `audit_log`
-  ADD PRIMARY KEY (`LogID`),
-  ADD KEY `UserID` (`UserID`);
 
 --
 -- Indexes for table `cart`
@@ -270,7 +251,8 @@ ALTER TABLE `cart`
 -- Indexes for table `coupon`
 --
 ALTER TABLE `coupon`
-  ADD PRIMARY KEY (`CouponID`);
+  ADD PRIMARY KEY (`CouponID`),
+  ADD UNIQUE KEY `Code` (`Code`);
 
 --
 -- Indexes for table `messages`
@@ -285,8 +267,7 @@ ALTER TABLE `messages`
 --
 ALTER TABLE `orders`
   ADD PRIMARY KEY (`OrderID`),
-  ADD KEY `UserID` (`UserID`),
-  ADD KEY `orders_ibfk_2` (`StatusID`);
+  ADD KEY `UserID` (`UserID`);
 
 --
 -- Indexes for table `order_items`
@@ -307,13 +288,14 @@ ALTER TABLE `order_status`
 --
 ALTER TABLE `product`
   ADD PRIMARY KEY (`ProductID`),
-  ADD KEY `SellerID` (`SellerID`),
+  ADD KEY `SellerID` (`SellerID`);
 
 --
 -- Indexes for table `system_settings`
 --
 ALTER TABLE `system_settings`
-  ADD PRIMARY KEY (`SettingName`);
+  ADD PRIMARY KEY (`SettingID`),
+  ADD UNIQUE KEY `SettingName` (`SettingName`);
 
 --
 -- Indexes for table `transaction`
@@ -326,7 +308,9 @@ ALTER TABLE `transaction`
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`UserID`);
+  ADD PRIMARY KEY (`UserID`),
+  ADD UNIQUE KEY `Username` (`Username`),
+  ADD UNIQUE KEY `Email` (`Email`);
 
 --
 -- Indexes for table `wallet`
@@ -336,21 +320,8 @@ ALTER TABLE `wallet`
   ADD KEY `UserID` (`UserID`);
 
 --
--- Indexes for table `wallet_transaction`
---
-ALTER TABLE `wallet_transaction`
-  ADD PRIMARY KEY (`TransactionID`),
-  ADD KEY `WalletID` (`WalletID`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `audit_log`
---
-ALTER TABLE `audit_log`
-  MODIFY `LogID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `cart`
@@ -368,7 +339,7 @@ ALTER TABLE `coupon`
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `MessageID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
+  MODIFY `MessageID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `orders`
@@ -386,13 +357,19 @@ ALTER TABLE `order_items`
 -- AUTO_INCREMENT for table `order_status`
 --
 ALTER TABLE `order_status`
-  MODIFY `StatusID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `StatusID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `ProductID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ProductID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `system_settings`
+--
+ALTER TABLE `system_settings`
+  MODIFY `SettingID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `transaction`
@@ -410,23 +387,11 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `wallet`
 --
 ALTER TABLE `wallet`
-  MODIFY `WalletID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `wallet_transaction`
---
-ALTER TABLE `wallet_transaction`
-  MODIFY `TransactionID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `WalletID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
 --
-
---
--- Constraints for table `audit_log`
---
-ALTER TABLE `audit_log`
-  ADD CONSTRAINT `audit_log_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`);
 
 --
 -- Constraints for table `cart`
@@ -439,15 +404,14 @@ ALTER TABLE `cart`
 -- Constraints for table `messages`
 --
 ALTER TABLE `messages`
-  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`SenderID`) REFERENCES `user` (`UserID`) ON DELETE CASCADE,
-  ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`ReceiverID`) REFERENCES `user` (`UserID`) ON DELETE CASCADE;
+  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`SenderID`) REFERENCES `user` (`UserID`),
+  ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`ReceiverID`) REFERENCES `user` (`UserID`);
 
 --
 -- Constraints for table `orders`
 --
 ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`),
-  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`StatusID`) REFERENCES `order_status` (`StatusID`);
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`);
 
 --
 -- Constraints for table `order_items`
@@ -460,7 +424,7 @@ ALTER TABLE `order_items`
 -- Constraints for table `product`
 --
 ALTER TABLE `product`
-  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`SellerID`) REFERENCES `user` (`UserID`),
+  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`SellerID`) REFERENCES `user` (`UserID`);
 
 --
 -- Constraints for table `transaction`
@@ -473,12 +437,6 @@ ALTER TABLE `transaction`
 --
 ALTER TABLE `wallet`
   ADD CONSTRAINT `wallet_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`);
-
---
--- Constraints for table `wallet_transaction`
---
-ALTER TABLE `wallet_transaction`
-  ADD CONSTRAINT `wallet_transaction_ibfk_1` FOREIGN KEY (`WalletID`) REFERENCES `wallet` (`WalletID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
